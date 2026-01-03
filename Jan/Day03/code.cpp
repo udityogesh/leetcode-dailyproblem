270a7caf3cfb5ac3639bb3d3e1b15b492616972b
udityogesh <yogeshwars854@gmail.com>
2026-01-03 22:00:47 +0530

    Implement numOfWays function in Solution class

---
+++ Jan/Day03/code.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numOfWays(int n) {
+        const int MOD = 1e9 + 7;
+
+        long same = 6; // ABA
+        long diff = 6; // ABC
+
+        for (int i = 2; i <= n; i++) {
+            long newSame = (same * 3 + diff * 2) % MOD;
+            long newDiff = (same * 2 + diff * 2) % MOD;
+            same = newSame;
+            diff = newDiff;
+        }
+
+        return (same + diff) % MOD;
+    }
+};
