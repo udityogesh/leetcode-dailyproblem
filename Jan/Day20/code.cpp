299692dd8e361e2c151b33c53bb284851822f636
udityogesh <yogeshwars854@gmail.com>
2026-01-20 23:14:45 +0530

    Add minBitwiseArray function to Solution class

---
+++ Jan/Day20/code.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>ans;
+
+        for(int i= 0 ; i < n ;i++){
+            bool found = false;
+            for(int j = 0 ; j < nums[i];j++){
+                if((j|j+1) == nums[i]){
+                    ans.push_back(j);
+                    found = true;
+                    break;
+                }
+            }
+            if(found == false){
+                ans.push_back(-1);
+            }
+        }
+        return ans;
+    }
+};
